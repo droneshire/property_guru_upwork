6db66c4e51d2b878d2dc5b8604b7ccfe1f7bdd99
Ross Yeager <ryeager12@gmail.com>
2023-07-03 17:00:47 -0700

    pidfile and linting main exe

--- src/exectutables/property_guru_bot.py
+++ src/exectutables/property_guru_bot.py
@@ -8,18 +8,37 @@ from util import log
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Property Guru Bot")
-    parser.add_argument("--env", type=str, default="dev", help="Environment to run the bot in")
+
+    log_dir = log.get_logging_dir("property_guru_bot")
+
+    parser.add_argument("--log-dir", default=log_dir)
+    parser.add_argument(
+        "--log-level",
+        type=str,
+        help="Logging level",
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Run the script without actually sending SMS",
+    )
+
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
 
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    src_dir = os.path.dirname(this_dir)
-    dotenv_dir = os.path.dirname(src_dir)
+    dotenv.load_dotenv(".env")
 
-    dotenv.load_dotenv(dotenv_path=os.path.join(dotenv_dir, ".env"))
+    bot_pidfile = os.environ.get("BOT_PIDFILE", "monitor_inventory.pid")
+
+    with open(bot_pidfile, "w", encoding="utf-8") as outfile:
+        outfile.write(str(os.getpid()))
+
+    log.setup_log(args.log_level, args.log_dir, "prop_guru")
 
     log.print_ok_blue("Starting Property Guru Bot")
 
