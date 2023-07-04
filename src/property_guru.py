85a1cca15ce6c266c8dd31f106248af17c0c02b5
Ross Yeager <ryeager12@gmail.com>
2023-07-03 17:29:54 -0700

    minor framework

---
+++ src/property_guru.py
@@ -0,0 +1,12 @@
+
+
+class PropertyGuru:
+
+    def __init__(self):
+        pass
+
+    def update_parameters(self, parameters: T.Dict[str, T.Any]) -> None:
+        pass
+
+    def check_properties(self) -> None:
+        pass
--- src/util/telegram_util.py
+++ src/util/telegram_util.py
@@ -1,3 +1,21 @@
 import asyncio
 
 import telegram
+
+
+class TelegramUtil:
+
+    def __init__(self, token: str) -> None:
+        self.bot = telegram.Bot(token=token)
+
+    async def check_token(self) -> bool:
+        async with self.bot:
+            try:
+                await self.bot.get_me()
+                return True
+            except telegram.error.Unauthorized:
+                return False
+
+    async def send_message(self, chat_id: str, message: str) -> None:
+        async with self.bot:
+            await self.bot.send_message(chat_id=chat_id, text=message)
