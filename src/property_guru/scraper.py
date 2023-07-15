01ed6c4673db8c518a70aa2357e891b61c6807b1
Ross Yeager <ryeager12@gmail.com>
2023-07-15 10:14:53 -0700

    linting

--- src/property_guru/scraper.py
+++ src/property_guru/scraper.py
@@ -1,6 +1,7 @@
 import json
 
 from bs4 import BeautifulSoup
+from lxml import etree
 
 from property_guru.data_types import SearchParams
 from property_guru.urls import CreateSearch, PropertyForSale
@@ -26,8 +27,21 @@ class PropertyGuru:
             log.print_fail("No parameters provided!")
             return
 
-        response = self.web2.get_request(
-            url=PropertyForSale.URL,
-            params=dict(parameters),
-        )
-        print(response)
+        # response = self.web2.get_request(
+        #     url=PropertyForSale.URL,
+        #     params=dict(parameters),
+        # )
+
+        # if not response:
+        #     log.print_fail("Failed to get response!")
+        #     return
+
+        with open("/tmp/property_guru.html", "r", encoding="utf-8") as infile:
+            data = infile.read()
+        soup = BeautifulSoup(data, "html.parser")
+        dom = etree.HTML(str(soup))
+        listings_xml = dom.xpath('//*[@id="listings-container"]')[0]
+        with open("/tmp/property_guru_section.html", "w", encoding="utf-8") as outfile:
+            outfile.write(etree.tostring(listings_xml, encoding="unicode"))
+        print(len(listings_xml))
+        # print([etree.tostring(listings_xml[i], encoding="unicode") for i in range(len(listings_xml))])
